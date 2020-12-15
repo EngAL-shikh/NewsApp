97d927e1a9ad59cd94cd27f7e67387a5c33055aa
omar“git config --global user.email engalshikhomar@gmail.com“ <engomaralshikh@gmail.com>
2020-12-15 22:04:54 +0300

    api

--- app/src/main/java/com/engalshikh/news/NewsFetchr.kt
+++ app/src/main/java/com/engalshikh/news/NewsFetchr.kt
@@ -16,7 +16,7 @@ class NewsFetchr {
 
     init {
         val retrofit: Retrofit = Retrofit.Builder()
-                .baseUrl("http://192.168.1.5")
+                .baseUrl("http://192.168.1.2")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         newsApi = retrofit.create(NewsApi::class.java)
@@ -34,6 +34,71 @@ class NewsFetchr {
 
                 val newsResponse:NewsResponse? = response.body()
                 val news:List<News> = newsResponse?.newsR
+                        ?: mutableListOf()
+                Log.d("TAG", "Response received")
+                responseLiveData.value = news
+                Log.d("onResponse", news.toString())
+            }
+        })
+        return responseLiveData
+
+    }
+
+    fun fetchPolticNews():LiveData<List<News>> {
+        val responseLiveData: MutableLiveData<List<News>> = MutableLiveData()
+        val newsRequest: Call<NewsResponse> = newsApi.fetchtype(1)
+        newsRequest.enqueue(object : Callback<NewsResponse> {
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                Log.e("TAG", "Failed to fetch News", t)
+            }
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>
+            ) {
+
+                val newsResponse:NewsResponse? = response.body()
+                val news:List<News> = newsResponse?.newsT
+                        ?: mutableListOf()
+                Log.d("TAG", "Response received")
+                responseLiveData.value = news
+                Log.d("onResponse", news.toString())
+            }
+        })
+        return responseLiveData
+
+    }
+    fun sportPolticNews():LiveData<List<News>> {
+        val responseLiveData: MutableLiveData<List<News>> = MutableLiveData()
+        val newsRequest: Call<NewsResponse> = newsApi.fetchtype(2)
+        newsRequest.enqueue(object : Callback<NewsResponse> {
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                Log.e("TAG", "Failed to fetch News", t)
+            }
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>
+            ) {
+
+                val newsResponse:NewsResponse? = response.body()
+                val news:List<News> = newsResponse?.newsT
+
+                        ?: mutableListOf()
+                Log.d("TAG", "Response received")
+                responseLiveData.value = news
+                Log.d("onResponse", news.toString())
+            }
+        })
+        return responseLiveData
+
+    }
+    fun oddPolticNews():LiveData<List<News>> {
+        val responseLiveData: MutableLiveData<List<News>> = MutableLiveData()
+        val newsRequest: Call<NewsResponse> = newsApi.fetchtype(3)
+        newsRequest.enqueue(object : Callback<NewsResponse> {
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                Log.e("TAG", "Failed to fetch News", t)
+            }
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>
+            ) {
+
+                val newsResponse:NewsResponse? = response.body()
+                val news:List<News> = newsResponse?.newsT
                         ?: mutableListOf()
                 Log.d("TAG", "Response received")
                 responseLiveData.value = news
