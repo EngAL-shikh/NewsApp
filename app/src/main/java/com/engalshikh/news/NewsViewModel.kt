12786e7bc157da8200b431a6bd6e4de9806971ee
omar“git config --global user.email engalshikhomar@gmail.com“ <engomaralshikh@gmail.com>
2020-12-15 22:05:47 +0300

    Add sportLiveData Add polticLiveData Add oddtLiveData

--- app/src/main/java/com/engalshikh/news/NewsViewModel.kt
+++ app/src/main/java/com/engalshikh/news/NewsViewModel.kt
@@ -6,8 +6,14 @@ import androidx.lifecycle.ViewModel
 class NewsViewModel:ViewModel() {
 
     val newsLiveData: LiveData<List<News>>
+    val polticnewsLiveData: LiveData<List<News>>
+    val sportnewsLiveData: LiveData<List<News>>
+   val oddnewsLiveData: LiveData<List<News>>
     init {
         newsLiveData = NewsFetchr().fetchContents()
+        polticnewsLiveData = NewsFetchr().fetchPolticNews()
+        sportnewsLiveData = NewsFetchr().sportPolticNews()
+        oddnewsLiveData = NewsFetchr().oddPolticNews()
     }
 
 }
