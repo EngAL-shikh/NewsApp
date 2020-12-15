99a1e419000a6430a78fec2ca150faf181613292
omar“git config --global user.email engalshikhomar@gmail.com“ <engomaralshikh@gmail.com>
2020-12-15 21:54:32 +0300

    api

--- app/src/main/java/API/NewsApi.kt
+++ app/src/main/java/API/NewsApi.kt
@@ -4,6 +4,7 @@ import com.engalshikh.news.News
 import com.engalshikh.news.NewsResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface NewsApi {
@@ -11,7 +12,7 @@ interface NewsApi {
     @GET("/API/api/api_news.php")
     fun fetchContents(): Call<NewsResponse>
 
-    @GET("/API/api/api_news.php")
-    fun fetchSportNews(): Call<NewsResponse>
-
+    @GET("/API/api/api_news.php?")
+    fun fetchtype(@Query("type") type: Int): Call<NewsResponse>
+    //fun fetchtype(type:Int): Call<NewsResponse>
 }
