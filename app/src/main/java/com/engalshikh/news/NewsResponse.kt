e10e4b08f56f7ad3c0a86e7d9f2961bbe69b9f23
omar“git config --global user.email engalshikhomar@gmail.com“ <engomaralshikh@gmail.com>
2020-12-15 22:06:21 +0300

    add newsT

--- app/src/main/java/com/engalshikh/news/NewsResponse.kt
+++ app/src/main/java/com/engalshikh/news/NewsResponse.kt
@@ -6,4 +6,7 @@ import com.google.gson.annotations.SerializedName
 data class NewsResponse (
         @SerializedName("getAllRows")
       var newsR:List<News>
+        ,
+        @SerializedName("gettypeRows")
+      var newsT:List<News>
 )
